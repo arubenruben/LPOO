c93a230fad1216397c94ebb3faa2ba1333d09f5b
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-14 22:15:26 +0000

    Understanding Mockito phylosofy

--- TP3/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ TP3/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -2,14 +2,19 @@ package com.aor.numbers;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class ListAggregatorTest {
     private List <Integer> list;
+    private ListDeduplicator stubDeduplicator;
+    private List<Integer> expected;
+    private IListDeduplicator deduplicator;
 
     @Test
     public void sum() {
@@ -18,7 +23,7 @@ public class ListAggregatorTest {
 
         int sum = aggregator.sum();
 
-        assertEquals(14, sum);
+        assertEquals(9, sum);
     }
 
     @Test
@@ -28,7 +33,7 @@ public class ListAggregatorTest {
 
         int max = aggregator.max();
 
-        assertEquals(5, max);
+        assertEquals(4, max);
     }
 
     @Test
@@ -43,14 +48,16 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-    helper();
+
+        helper();
         ListAggregator aggregator = new ListAggregator(list);
 
-        int distinct = aggregator.distinct();
+        int distinct = aggregator.distinct(deduplicator);
 
-        assertEquals(4, distinct);
+        assertEquals(3, distinct);
     }
 
+    //1, 2, 4 and 2
     @Before
     public void helper(){
         this.list = new ArrayList<>();
@@ -58,7 +65,15 @@ public class ListAggregatorTest {
         this.list.add(2);
         this.list.add(4);
         this.list.add(2);
-        this.list.add(5);
-        
+
+
+        this.expected=new ArrayList<>();
+        this.expected.add(1);
+        this.expected.add(2);
+        this.expected.add(4);
+
+        this.deduplicator = Mockito.mock(IListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate()).thenReturn(this.expected);
+
     }
 }
